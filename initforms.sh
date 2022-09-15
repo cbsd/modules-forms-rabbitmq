1a003387baaf9d2515be7cee0076b7014d06fc1b
mergar <subbsd@gmail.com>
2022-09-15 22:29:56 +0300

    init admin user by default

--- initforms.sh
+++ initforms.sh
@@ -33,26 +33,28 @@ FORM_PATH="${workdir}/formfile"
 /usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms.schema additional_cfg
 /usr/local/bin/cbsd ${miscdir}/updatesql ${FORM_PATH}/${HELPER}.sqlite ${distsharedir}/forms_system.schema system
 
-${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite << EOF
+${SQLITE3_CMD} ${FORM_PATH}/${HELPER}.sqlite <<EOF
 BEGIN TRANSACTION;
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,1,"-Globals","Globals",'Globals','PP','',1, "maxlen=60", "delimer", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,3,"cluster_name","cluster_name",'myclu1','myclu1','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,4,"port","port",'5672','5672','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,4,"management_port","management_port",'15672','15672','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,5,"delete_guest_user","delete_guest_user",'true','true','',1, "maxlen=5", "inputbox", "", "" );
-#INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,6,"config_cluster","config_cluster",'true','true','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,7,"cluster_node_type","cluster_node_type",'2','ram','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,8,"admin_enable","admin_enable",'2','true','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,9,"tcp_recbuf","tcp_recbuf",'2','196608','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,10,"tcp_sndbuf","tcp_sndbuf",'2','196608','',1, "maxlen=5", "inputbox", "", "" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,200,"-Vhosts","Vhosts",'Vhosts','-','',1, "maxlen=60", "delimer", "", "vhostgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,201,"vhost","add vhosts",'201','','',0, "maxlen=60", "group_add", "", "vhostgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,300,"-Users","Users",'Users','-','',1, "maxlen=60", "delimer", "", "usersgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,301,"user","add users",'301','','',0, "maxlen=60", "group_add", "", "usersgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,400,"-Permissions","Permissions",'Permissions','-','',1, "maxlen=60", "delimer", "", "permissionsgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,401,"permission","add permissions",'401','','',0, "maxlen=60", "group_add", "", "permissionsgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,500,"-Plugins","Permissions",'Plugins','-','',1, "maxlen=60", "delimer", "", "pluginsgroup" );
-INSERT INTO forms ( mytable,group_id,order_id,param,desc,def,cur,new,mandatory,attr,type,link,groupname ) VALUES ( "forms", 1,501,"plugin","add plugins",'501','','',0, "maxlen=60", "group_add", "", "pluginsgroup" );
+INSERT INTO forms VALUES(1,'forms',1,1,'-Globals','Globals','Globals','PP','',1,'maxlen=60',NULL,'delimer','','');
+INSERT INTO forms VALUES(2,'forms',1,3,'cluster_name','cluster_name','myclu1','myclu1','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(3,'forms',1,4,'port','port','5672','5672','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(4,'forms',1,4,'management_port','management_port','15672','15672','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(5,'forms',1,5,'delete_guest_user','delete_guest_user','true','true','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(6,'forms',1,7,'cluster_node_type','cluster_node_type','2','ram','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(7,'forms',1,8,'admin_enable','admin_enable','2','true','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(8,'forms',1,9,'tcp_recbuf','tcp_recbuf','2','196608','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(9,'forms',1,10,'tcp_sndbuf','tcp_sndbuf','2','196608','',1,'maxlen=5',NULL,'inputbox','','');
+INSERT INTO forms VALUES(10,'forms',1,200,'-Vhosts','Vhosts','Vhosts','-','',1,'maxlen=60',NULL,'delimer','','vhostgroup');
+INSERT INTO forms VALUES(11,'forms',1,201,'vhost','add vhosts','201','201','',0,'maxlen=60',NULL,'group_add','','vhostgroup');
+INSERT INTO forms VALUES(12,'forms',1,300,'-Users','Users','Users','-','',1,'maxlen=60',NULL,'delimer','','usersgroup');
+INSERT INTO forms VALUES(13,'forms',1,301,'user','add users','301','301','',0,'maxlen=60',NULL,'group_add','','usersgroup');
+INSERT INTO forms VALUES(14,'forms',1,400,'-Permissions','Permissions','Permissions','-','',1,'maxlen=60',NULL,'delimer','','permissionsgroup');
+INSERT INTO forms VALUES(15,'forms',1,401,'permission','add permissions','401','401','',0,'maxlen=60',NULL,'group_add','','permissionsgroup');
+INSERT INTO forms VALUES(16,'forms',1,500,'-Plugins','Permissions','Plugins','-','',1,'maxlen=60',NULL,'delimer','','pluginsgroup');
+INSERT INTO forms VALUES(17,'forms',1,501,'plugin','add plugins','501','501','',0,'maxlen=60',NULL,'group_add','','pluginsgroup');
+INSERT INTO forms VALUES(18,'forms',2,301,'user_name2','user part 2','','admin','',1,'maxlen=60','dynamic','inputbox','','usersgroup');
+INSERT INTO forms VALUES(19,'forms',2,301,'user_password2','password part 2','simplepassword','admin','',1,'maxlen=60','dynamic','inputbox','','usersgroup');
+INSERT INTO forms VALUES(20,'forms',2,301,'user_admin2','admin part 2','false','true','',1,'maxlen=60','dynamic','inputbox','','usersgroup');
 COMMIT;
 EOF
 
